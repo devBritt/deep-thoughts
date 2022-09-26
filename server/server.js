c74a47de6020b3d4c74863fa17f3be6e00fddce9
Britt Carlton <brittanyrc95@gmail.com>
2022-09-26 15:19:59 -0400

    Implement ThoughtList for homepage

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -26,6 +27,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
     // integrate Apollo server with the Express application as middleware
     server.applyMiddleware({ app });
+
+    // serve up static assets
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
 
     db.once('open', () => {
     app.listen(PORT, () => {
