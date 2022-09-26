3ca256bc14143237e2dc5d65893d7d83bee36d19
Britt Carlton <brittanyrc95@gmail.com>
2022-09-26 13:02:19 -0400

    Implement addThought with authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -29,7 +29,7 @@ const resolvers = {
         },
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({})
+                const userData = await User.findOne({ _id: context.user._id})
                 .select('-__v -password')
                 .populate('thoughts')
                 .populate('friends');
@@ -62,6 +62,21 @@ const resolvers = {
 
             const token = signToken(user);
             return { token, user };
+        },
+        addThought: async (parent, args, context) => {
+            if (context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { thoughts: thought._id } },
+                    { new: true }
+                );
+
+                return thought;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
