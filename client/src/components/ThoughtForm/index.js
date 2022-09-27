5e6ebfa3d75ccefa2ba9845be40e780fa82670b1
Britt Carlton <brittanyrc95@gmail.com>
2022-09-27 12:43:17 -0400

    Add onChange, onSubmit callback functions to ThoughtForm

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 
 const ThoughtForm = () => {
+    // keep track of entered text and character count
+    const [thoughtText, setText] = useState('');
+    const [characterCount, setCharCount] = useState(0);
+
+    // function to handle textarea value changes
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setText(event.target.value);
+            setCharCount(event.target.value.length);
+        }
+    }
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        setText('');
+        setCharCount(0);
+    }
+
     return (
         <div>
-            <p className='m-0'>
-                Character Count: 0/280
+            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}'`}>
+                Character Count: {characterCount}/280
             </p>
-            <form className='flex-row justify-center justify-space-between-md align-stretch'>
+            <form className='flex-row justify-center justify-space-between-md align-stretch' onSubmit={handleFormSubmit}>
                 <textarea
                     placeholder="Here's a new thought..."
+                    value={thoughtText}
                     className='form-input col-12 col-md-9'
+                    onChange={handleChange}
                 ></textarea>
                 <button className='btn col-12 col-md-3' type='submit'>
                     Submit
